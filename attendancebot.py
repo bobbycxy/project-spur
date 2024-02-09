7f5aa2f9c54e84d76180b2e9b8cefc211dc28d07
bobbycxy <bobbycxy1994@gmail.com>
2024-02-09 12:31:44 +0800

    revision 2

--- attendancebot.py
+++ attendancebot.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 from typing import Dict
 import creds
 
@@ -24,44 +25,107 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+LOGIN_REPLY, CHOOSING_CELL, CHOOSING_MONTH, CHOOSING_DAY, CHOOSING_MEMBERS, REMOVING_MEMBERS = range(6)
 
-reply_keyboard = [[item] for item in db.get_cell_groups()]
+reply_keyboard = sorted([[item] for item in db.get_cell_groups()])
 markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
 
+## helper function
 def facts_to_str(user_data: Dict[str, str]) -> str:
     """Helper function for formatting the gathered user info."""
-    facts = [f"{key} - {value}" for key, value in user_data.items()]
+    facts = [f"{key}: {value}\n" for key, value in user_data.items() if key != 'Attendees'] + [f"{key} ({len(value)}):" for key, value in user_data.items() if key == 'Attendees']
+
+    if 'Attendees' in user_data.keys():
+        for n, item in enumerate(user_data['Attendees']):
+            facts.append(f'{n+1}. {item}')
+
     return "\n".join(facts).join(["\n", "\n"])
 
 
+## /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Start the conversation and ask user for input."""
+    """Start the conversation and ask user for verification."""
     await update.message.reply_text(
         "Hi! This is an attendance bot for PoD, the youth ministry of COSB."
-        "What cell group are we taking attendance for?",
+        " In order to verify our users, kindly insert verification code."
+    )
+
+    return LOGIN_REPLY
+
+
+## /select_cell
+async def select_cell(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Ask user to select cell group"""
+    await update.message.reply_text(
+        "Welcome!"
+        " What cell group are we taking attendance for?",
         reply_markup=markup,
     )
 
-    return CHOOSING
+    return CHOOSING_CELL
+
+
+## selecting the month
+async def select_month(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Ask user for the month selection."""
+    text = update.message.text
+    context.user_data["Cell"] = text
+
+    ## prepare a keyboard for the number of months
+    reply_keyboard = [['Jan','Feb','Mar'],['Apr','May','Jun'],['Jul','Aug','Sep'],['Oct','Nov','Dec']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text(
+        f"You have selected {text}!"
+        " What month are we taking attendance for?",
+        reply_markup=markup,
+    )
+
+    return CHOOSING_MONTH
+
+
+## selecting the day
+async def select_day(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Ask user for the day selection."""
+    text = update.message.text
+    context.user_data["month"] = text
+    print(context.user_data["Cell"],db.get_cell_members(context.user_data["Cell"]))
+
+    ## prepare a keyboard for the number of months
+    reply_keyboard = [['1','2','3'],['4','5','6'],['7','8','9'],['10','11','12'],['13','14','15'],['16','17','18'],['19','20','21'],['22','23','24'],['25','26','27'],['28','29','30'],['31']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text(
+        f"You have selected {context.user_data['Cell']}, and are taking attendance for the month of {text}!"
+        " What day are we taking attendance for?",
+        reply_markup=markup,
+    )
+
+    return CHOOSING_DAY
 
 
 async def regular_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Ask the user for info about the selected predefined choice."""
     text = update.message.text
-    context.user_data["choice"] = text
-    await update.message.reply_text(f"You have selected {text.lower()}! Who is present today")
+    context.user_data["day"] = text
+    cell_members = db.get_cell_members(context.user_data["Cell"])
+    attendance_date = str(datetime.now().year) + f'-{context.user_data["month"]}-{context.user_data["day"]}'
+    context.user_data["Date"] = attendance_date
 
-    reply_keyboard = [[name] for name in db.get_cell_members(text)]
+    del context.user_data["month"]
+    del context.user_data["day"]
+
+    reply_keyboard = sorted([[name] for name in cell_members]) + [['REMOVE','DONE']]
     markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
     await update.message.reply_text(
-        "Who are the members who attended?",
+        f"You have selected {context.user_data['Cell']}, for the date of {attendance_date}."
+        " Who are the members who attended?",
         reply_markup=markup,
     )
 
-    return TYPING_REPLY
+    return CHOOSING_MEMBERS
 
 
 async def received_information(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -70,32 +134,85 @@ async def received_information(update: Update, context: ContextTypes.DEFAULT_TYP
     user_data = context.user_data
     print(user_data)
     text = update.message.text
-    category = user_data["choice"]
-    if category not in user_data.keys():
-        user_data[category] = []
-    user_data[category].append(text)
+    if text != 'DONE':
+        if 'Attendees' not in user_data.keys():
+            user_data['Attendees'] = []
+        if text not in user_data['Attendees']:
+            user_data['Attendees'].append(text)
 
-    del user_data["choice"]
+    # del user_data["choice"]
+
+    cell_members = db.get_cell_members(user_data["Cell"])
+
+    reply_keyboard = sorted([[name] for name in cell_members]) + [['REMOVE','DONE']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
 
     await update.message.reply_text(
-        "Neat! Just so you know, this is what you already told me:"
-        f"{facts_to_str(user_data)}You can tell me more, or change your opinion"
-        " on something.",
+        "Neat! Just so you know, this is what you already told me:\n"
+        f"{facts_to_str(user_data)}\nAny more members?",
         reply_markup=markup,
     )
 
-    return CHOOSING
+    return CHOOSING_MEMBERS
+
+
+async def remove(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Store info provided by user and ask for the next category."""
+
+    user_data = context.user_data
+
+    # del user_data["choice"]
+
+    attendees = user_data['Attendees']
+
+    reply_keyboard = [[name] for name in attendees] + [['DONE']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text(
+        "Got it, you want to remove some names.\n"
+        f"{facts_to_str(user_data)}\nWho would you like to remove?",
+        reply_markup=markup,
+    )
+
+    return REMOVING_MEMBERS
+
+async def remove_update(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Store info provided by user and ask for the next category."""
+
+    user_data = context.user_data
+    text = update.message.text
+    user_data['Attendees'].remove(text)
+    # del user_data["choice"]
+
+    attendees = user_data['Attendees']
+
+    reply_keyboard = [[name] for name in attendees] + [['DONE']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+    await update.message.reply_text(
+        "Okay, I've removed the member.\n"
+        f"{facts_to_str(user_data)}\nWho else would you like to remove?\nIf you have finished removing, press 'DONE'",
+        reply_markup=markup,
+    )
+
+    return REMOVING_MEMBERS
+
 
 async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Display the gathered info and end the conversation."""
     user_data = context.user_data
-    if "choice" in user_data:
-        del user_data["choice"]
+    # if "choice" in user_data:
+    #     del user_data["choice"]
 
     await update.message.reply_text(
-        f"I learned these facts about you: {facts_to_str(user_data)}Until next time!",
+        f"I collected these information:\n {facts_to_str(user_data)}\nI will proceed to update their attendance now.",
         reply_markup=ReplyKeyboardRemove(),
     )
+
+    attendance_date = datetime.strptime(user_data['Date'], '%Y-%b-%d')
+
+    for attendee in user_data['Attendees']:
+        db.add_attendance(user_data['Cell'], attendance_date, attendee)
 
     user_data.clear()
     return ConversationHandler.END
@@ -109,25 +226,36 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            CHOOSING: [
+            LOGIN_REPLY: [
                 MessageHandler(
-                    filters.Regex("^(ONE|Bouquet|Kadesh|Gilead)$"), regular_choice
+                    filters.Regex("^(podYouths#159)$"), select_cell
+                )            ],
+            CHOOSING_CELL: [
+                MessageHandler(
+                    filters.Regex("^(ONE|Bouquet|Kadesh|Gilead)$"), select_month
+                )            ],
+            CHOOSING_MONTH: [
+                MessageHandler(
+                    filters.Regex("^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)$"), select_day
+                )            ],
+            CHOOSING_DAY: [
+                MessageHandler(
+                    filters.Regex("^(1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31)$"), regular_choice
+                )            ],
+            CHOOSING_MEMBERS: [
+                MessageHandler(
+                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^DONE$") | filters.Regex("^REMOVE$")), received_information
                 ),
-                # MessageHandler(filters.Regex("^Something else...$"), custom_choice),
+                MessageHandler(filters.Regex("^REMOVE$"), remove),
             ],
-            TYPING_CHOICE: [
+            REMOVING_MEMBERS: [
                 MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Done$")), regular_choice
-                )
-            ],
-            TYPING_REPLY: [
-                MessageHandler(
-                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^Done$")),
-                    received_information,
-                )
+                    filters.TEXT & ~(filters.COMMAND | filters.Regex("^DONE$")), remove_update
+                ),
+                MessageHandler(filters.Regex("^DONE$"), received_information),
             ],
         },
-        fallbacks=[MessageHandler(filters.Regex("^Done$"), done)],
+        fallbacks=[MessageHandler(filters.Regex("^DONE$"), done)],
     )
 
     application.add_handler(conv_handler)
